a8325e9f0bf613948fec60a8b630696504f3b250
Piotr Kozubek <piotrkozubek@gmail.com>
2017-11-11 15:24:21 +0100

    Improvements to displaying articles on main page

--- src/connectors/HomePage.js
+++ src/connectors/HomePage.js
@@ -1,14 +1,15 @@
 import {connect} from "react-redux";
+import Immutable from "immutable";
 import HomePage from "../components/HomePage";
 import {getDoc} from "../actions/actions";
 
 function mapStateToProps(state) {
   return {
-    reading: state.getIn(["app", "defaultDoc", "reading"], ""),
-    understanding: state.getIn(["app", "defaultDoc", "understanding"], ""),
-    anticipating: state.getIn(["app", "defaultDoc", "anticipating"], ""),
-    thinking: state.getIn(["app", "defaultDoc", "thinking"], ""),
-    memorizing: state.getIn(["app", "defaultDoc", "memorizing"], ""),
+    reading: state.getIn(["app", "defaultDoc", "reading"], Immutable.Map()),
+    understanding: state.getIn(["app", "defaultDoc", "understanding"], Immutable.Map()),
+    anticipating: state.getIn(["app", "defaultDoc", "anticipating"], Immutable.Map()),
+    thinking: state.getIn(["app", "defaultDoc", "thinking"], Immutable.Map()),
+    memorizing: state.getIn(["app", "defaultDoc", "memorizing"], Immutable.Map()),
   };
 }
 
