aa380a1cc7a4732140c237cd680bf92a31b5411f
Piotr Kozubek <piotrkozubek@gmail.com>
2017-11-22 11:25:14 +0100

    Improvements to modal window

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,55 +1,48 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import cn from 'classnames';
-import {bool, object, string, func} from "prop-types";
+import { bool, node, string, func, oneOf } from "prop-types";
 
 const ModalContent = ({
+  isOpen,
   closeBtn,
   onClose,
   children,
   title,
   overlay,
   position,
+  container,
 }) => {
   return (
-    <div className="modal">
-      <div className={cn({"modal__overlay": overlay})}>
-        <div className={cn("modal__content", position)}>
-          <h3>{title}</h3>
-          {children}
+    isOpen ? ReactDOM.createPortal(
+      <div className="modal">
+        <div className={cn('modal-container', {"modal__overlay": overlay})}>
+          <div className={cn("modal__content", position)}>
+            {closeBtn && <button onClick={onClose} className="modal--close" />}
+            <h3>{title}</h3>
+            {children}
+          </div>
         </div>
-      </div>
-    </div>
+      </div>, container || document.getElementById("root"))
+    : null
   );
 };
 
-export default class extends React.Component {
+ModalContent.propTypes = {
+  isOpen: bool.isRequired,
+  closeBtn: bool.isRequired,
+  onClose: func.isRequired,
+  children: node.isRequired,
+  container: node,
+  title: string,
+  overlay: bool,
+  position: oneOf(["center", "left", "right"]).isRequired,
+};
 
-  static propTypes = {
-    isOpen: bool.isRequired,
-    contentLabel: string,
-    closeModal: func.isRequired,
-    closeBtn: bool,
-    style: object,
-  }
+ModalContent.defaultProps = {
+  title: "",
+  overlay: true,
+  container: null,
+};
 
-  static defaultProps = {
-    contentLabel: "",
-    closeBtn: true,
-    style: {},
-  }
-
-  render() {
-    const { isOpen, children, container, overlay, title, position } = this.props;
-
-    return (
-      isOpen ? ReactDOM.createPortal(<ModalContent
-          overlay={overlay}
-          title={title}
-          position={position}
-        >
-        {children}
-      </ModalContent>, container) : null
-    );
-  }
-}
+export default ModalContent;
--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import Modal from '../common/Modal';
 
-const Settings = ({ }) => {
-  return (
-    <div>
-      <h2>Settings page</h2>
-      <Modal
-        isOpen
-        overlay={true}
-        container={document.getElementById("root")}
-        title="Modal with settings"
-        position="center"
-      >
-        <div className="test">
-          <h3>Lorem ipsum from onet</h3>
-          <div>Dzisiaj do godziny 13.00 patrole obrońców przyrody tylko raz natknęły się na ciężki sprzęt pracujący przy wycince drzew. Jednak na widok ekologów maszyny zaprzestały pracy i wróciły na parking. Aktywistom z Obozu dla Puszczy jednak nie udało się zlokalizować maszyn z innego parkingu.</div>
-        </div>
-      </Modal>
-    </div>
-  );
+class Settings extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: true,
+    };
+
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalOpen: false});
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 onClick={this.openModal}>Settings page</h2>
+        <Modal
+          isOpen={this.state.modalOpen}
+          overlay={true}
+          title="Modal with settings"
+          position="right"
+          closeBtn={true}
+          onClose={this.closeModal}
+        >
+          <div className="test">
+            <h3>Lorem ipsum from onet</h3>
+            <div>Dzisiaj do godziny 13.00 patrole obrońców przyrody tylko raz natknęły się na ciężki sprzęt pracujący przy wycince drzew. Jednak na widok ekologów maszyny zaprzestały pracy i wróciły na parking. Aktywistom z Obozu dla Puszczy jednak nie udało się zlokalizować maszyn z innego parkingu.</div>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
 };
 
 export default Settings;
